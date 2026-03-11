0b66ac9a6546795e3e2b773123ea173da291cbc6
burak.bolek@authoritypartners.com-at-930184570490 <burak.bolek@authoritypartners.com>
2026-03-11 22:11:04 +0100

    fix: correct variable name for model in process launch output

--- profiles/default/systems/ui/modules/ProcessAPI.psm1
+++ profiles/default/systems/ui/modules/ProcessAPI.psm1
@@ -346,7 +346,7 @@ function Start-ProcessLaunch {
         process_id = $launchedProcId
         pid = $proc.Id
         type = $Type
-        model = $launchModel
+        model = $Model
     }
 }
 
